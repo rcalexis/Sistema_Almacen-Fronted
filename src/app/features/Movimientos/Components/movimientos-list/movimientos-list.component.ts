e88bcdc96220f24bc3c5ee6bb21452a5054c396e
AlexisR <rcaalexis10@gmail.com>
2025-10-19 23:17:48 -0600

    feat: creacion de componente movimientos-list

---
+++ src/app/features/Movimientos/Components/movimientos-list/movimientos-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimientos-list',
+  imports: [],
+  templateUrl: './movimientos-list.component.html',
+  styleUrl: './movimientos-list.component.css'
+})
+export class MovimientosListComponent {
+
+}
