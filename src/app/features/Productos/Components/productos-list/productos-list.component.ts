76fd5dee6ea56e6d2a71c554b63b8736d27363e1
AlexisR <rcaalexis10@gmail.com>
2025-10-19 23:09:42 -0600

    feat: creacion de componente producto-list

---
+++ src/app/features/Productos/Components/productos-list/productos-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-list',
+  imports: [],
+  templateUrl: './productos-list.component.html',
+  styleUrl: './productos-list.component.css'
+})
+export class ProductosListComponent {
+
+}
