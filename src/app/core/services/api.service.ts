8be07f2a584000f1c5eb4c0c5f4496703ffe978d
AlexisR <rcaalexis10@gmail.com>
2025-10-19 22:43:26 -0600

    feat: creacion de carpeta core con implementacion de servicio para peticiones HTTP

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) { }
+
+  getItems(Module: string, Accion: string = ''): Observable<any[]> {
+    let AccionValor = Accion != '' ? `/${Accion}`:'';
+    return this.http.get<any[]>(`${this.apiUrl}/${Module}${this.accionValor(Accion)}`);
+  }
+
+  getItem(Module: string, id: number, Accion: string = ''): Observable<any> {
+    let AccionValor = Accion != '' ? `/${Accion}`: '';
+    return this.http.get<any>(`${this.apiUrl}/${Module}${this.accionValor(Accion)}/${id}`);
+  }
+
+  postItem(Module: string, data: any, Accion: string = ''): Observable<any> {
+    let AccionValor = Accion != '' ? `/${Accion}` :'';
+    return this.http.post<any>(`${this.apiUrl}/${Module}${this.accionValor(Accion)}`,data);
+  }
+
+  updateItem(Module: string, id: number, data: any, Accion: string = ''): Observable<any> {
+    let AccionValor = Accion != '' ? `/${Accion}` : '';
+    return this.http.put<any>(`${this.apiUrl}/${Module}${this.accionValor(Accion)}/${id}`, data);
+  }
+
+  deleteItem(Module: string, id: number, Accion: string = ''): Observable<any> {
+    let AccionValor = Accion != '' ? `/${Accion}`: '';
+    return this.http.delete<any>(`${this.apiUrl}/${Module}${this.accionValor(Accion)}/${id}`);
+  }
+
+  accionValor(Accion: string){
+    return Accion != '' ? `/${Accion}`: '';
+  }
+}
