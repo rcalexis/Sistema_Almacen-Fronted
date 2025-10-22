aab435dfcfb2a0b5ba55bb13d0980ae83db73441
AlexisR <rcaalexis10@gmail.com>
2025-10-21 22:23:50 -0600

    feat(features): funcionalidad login

--- src/app/features/Auth/Components/login/login.component.ts
+++ src/app/features/Auth/Components/login/login.component.ts
@@ -1,11 +1,58 @@
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { ApiService } from '../../../../core/services/api.service';
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    DividerModule,
+    InputTextModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loginError: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched(); 
+      return;
+    }
+
+    this.loginError = null; 
+
+    this.apiService.postItem('login', this.loginForm.value).subscribe({
+      next: (response) => {
+
+        console.log('Login exitoso:', response);
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('usuario', JSON.stringify(response.usuario));
+        this.router.navigateByUrl('/products');
+      },
+      error: (err) => {
+        console.error('Error en el login:', err);
+        this.loginError = err.error?.mensaje || 'Error al intentar iniciar sesion';
+      }
+    });
+  }
 
 }
