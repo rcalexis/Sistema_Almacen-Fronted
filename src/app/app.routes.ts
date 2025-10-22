0b5b3c19e864bcfedcfaa5bab8739022db083bd0
AlexisR <rcaalexis10@gmail.com>
2025-10-21 22:25:38 -0600

    feat(routes): Ruta para login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/Auth/components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+ { path: 'login', component: LoginComponent },
+
+ 
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+
+  { path: '**', redirectTo: '/login' }
+];
