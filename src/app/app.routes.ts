b77dbeafa21a14f7c007384727e6846052ec53f7
AlexisR <rcaalexis10@gmail.com>
2025-10-21 22:29:14 -0600

    feat(routes): Ruta para componente Productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
+import { ProductosListComponent } from './features/Productos/Components/productos-list/productos-list.component';
+import { authGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './features/Auth/components/login/login.component';
+import { ProductosFormComponent } from './features/Productos/Components/productos-form/productos-form.component';
 
 export const routes: Routes = [
 
  { path: 'login', component: LoginComponent },
 
+ {
+    path: 'products',
+    canActivate: [authGuard],
+    children: [
+      { path: '', component: ProductosListComponent },
+      { path: 'crear', component: ProductosFormComponent }, 
+    ]
+  },
  
+
   { path: '', redirectTo: '/products', pathMatch: 'full' },
 
   { path: '**', redirectTo: '/login' }
