37b06324628f56126358463ab630b92629f6a3eb
AlexisR <rcaalexis10@gmail.com>
2025-10-22 21:06:33 -0600

    feat: implementacion de navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+
+import { filter, Subscription } from 'rxjs';
+import { NavbarComponent } from './Layout/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ButtonModule],
+  imports: [CommonModule,RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'FrontAlmacen';
+  private router = inject(Router);
+  
+  showNavbar = true;
+  private routerSubscription: Subscription;
+
+  constructor() {
+    
+    this.routerSubscription = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: any) => {
+        this.updateNavbarVisibility(event.url);
+      });
+
+    
+    this.updateNavbarVisibility(this.router.url);
+  }
+
+  private updateNavbarVisibility(url: string): void {
+   
+    this.showNavbar = !(url === '/login' || url === '/');
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 }
